ba9d9960ab41943e457dafed763383e3f8b9f6b1
yuvam2017 <dryuvam14900@gmail.com>
2023-03-02 21:58:36 +0530

    third

--- q4.py
+++ q4.py
@@ -1,13 +1,15 @@
-def monoalphabetic_substitution(text, key, encrypt=True):
-    # Create a dictionary to map characters to their corresponding key values
-    key_dict = dict(zip(key, range(len(key))))
+import string
 
-    # Inverse key dictionary for decryption
-    inv_key_dict = dict(zip(range(len(key)), key))
-
+def monoalphabetic_cipher(text, key, encrypt=True):
+    
+    alphabet = string.ascii_lowercase
+    key_dict = dict(zip(alphabet, key))
+    inv_key_dict = {v: k for k, v in key_dict.items()}
+  
     result = ""
+   
     for char in text:
-        if char.isalpha():
+        if char.lower() in alphabet:
             if encrypt:
                 result += key_dict[char.lower()].upper() if char.isupper() else key_dict[char]
             else:
@@ -17,39 +19,38 @@ def monoalphabetic_substitution(text, key, encrypt=True):
     return result
 
 def polyalphabetic_substitution(text, key, encrypt=True):
-    # Create a list of keys 
-    key_list = [key[i % len(key)] for i in range(len(text))]
-
-    # Create a dictionary 
-    key_dict = dict(zip(key, range(len(key))))
-
-    # Inverse key dictionary for decryption
-    inv_key_dict = dict(zip(range(len(key)), key))
-
     result = ""
     for i, char in enumerate(text):
         if char.isalpha():
+            key_index = i % len(key)
+            shift = ord(key[key_index].upper()) - ord('A')
             if encrypt:
-                result += inv_key_dict[(key_dict[char.lower()] + key_dict[key_list[i].lower()]) % len(key)].upper() if char.isupper() else inv_key_dict[(key_dict[char] + key_dict[key_list[i]]) % len(key)]
+                result += chr((ord(char.upper()) + shift - 2*ord('A')) % 26 + ord('A'))
             else:
-                result += inv_key_dict[(key_dict[char.lower()] - key_dict[key_list[i].lower()]) % len(key)].upper() if char.isupper() else inv_key_dict[(key_dict[char] - key_dict[key_list[i]]) % len(key)]
+                result += chr((ord(char.upper()) - shift - 2*ord('A')) % 26 + ord('A'))
         else:
             result += char
     return result
 
-if __name__ == "__main__":
-    # Monoalphabetic Substitution Cipher
-    plain_text = input("Enter the plain text: ")
-    key = input("Enter the key: ")
-    cipher_text = monoalphabetic_substitution(plain_text, key)
-    print("Cipher Text: ", cipher_text)
-    decipher_text = monoalphabetic_substitution(cipher_text, key, encrypt=False)
-    print("Deciphered Text: ", decipher_text)
 
-    # Polyalphabetic Substitution Cipher
-    plain_text = input("Enter the plain text: ")
-    key = input("Enter the key: ")
-    cipher_text = polyalphabetic_substitution(plain_text, key)
-    print("Cipher Text: ", cipher_text)
-    decipher_text = polyalphabetic_substitution(cipher_text, key, encrypt=False)
-    print("Deciphered Text: ", decipher_text)
+if __name__ == "__main__":
+    print("MONOALPHABETIC CIPHER SUBSTITUTION")
+    plaintext = input("Enter the Text : ")
+    key = input("Enter the Key (or press Enter for the default key) : ")
+    key = "phqgiumeaylnofdxjkrcvstzwb" if key == "" else key
+    ciphertext = monoalphabetic_cipher(plaintext, key)
+    print("Plaintext:", plaintext)
+    print("Ciphertext:", ciphertext)
+    decrypted_text = monoalphabetic_cipher(ciphertext, key, encrypt=False)
+    print("Decrypted text:", decrypted_text)
+
+
+    print("\n\nPOLYALPHABETIC CIPHER SUBSTITUTION")
+    plaintext = input("Enter the Text : ")
+    key = input("Enter the Key (or press Enter for the default key) : ")
+    key = "losientoadios" if key == "" else key
+    ciphertext = polyalphabetic_substitution(plaintext, key)
+    print("Plaintext:", plaintext)
+    print("Ciphertext:", ciphertext)
+    decrypted_text = polyalphabetic_substitution(ciphertext, key, encrypt=False)
+    print("Decrypted text:", decrypted_text)
